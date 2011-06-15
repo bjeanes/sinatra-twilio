839275d3a2b12d8667d3f0593be68d1c1298b92b
Bodaniel Jeanes <me@bjeanes.com>
2011-06-14 21:18:56 -0500

    Remove cruft which really only tested Sinatra behaviour

--- spec/sinatra/twilio_spec.rb
+++ spec/sinatra/twilio_spec.rb
@@ -1,41 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe Sinatra do
+describe Sinatra::Application do
+  let(:app) { Sinatra::Application }
   subject { app }
 
-  context "classic apps" do
-    let(:app) { Sinatra::Application }
-    it { should respond_to(:respond) }
-  end
+  describe "#respond" do
+    context "defining routes" do
+      let(:route) { "/foo" }
+      let(:action) { Proc.new {} }
 
-  describe "modular apps" do
-    let(:app) { Class.new(Sinatra::Base) { register Sinatra::Twilio } }
-    it { should respond_to(:respond) }
+      before { app.respond(route, &action) }
 
-    describe "#respond" do
-      context "defining routes" do
-        let(:route) { "/foo" }
-        let(:action) { Proc.new {} }
+      it "defines a GET route" do
+        app.routes["GET"].should have(1).item
+      end
 
-        before { app.respond(route, &action) }
+      it "defines a POST route" do
+        app.routes["POST"].should have(1).item
+      end
 
-        it "defines a GET route" do
-          app.routes["GET"].should have(1).item
-        end
+      it "should have identical GET and POST routes" do
+        t = Proc.new {|res| [res.status, res.headers, res.body] }
 
-        it "defines a POST route" do
-          app.routes["POST"].should have(1).item
-        end
-
-        it "should have identical GET and POST routes" do
-          t = Proc.new {|res| [res.status, res.headers, res.body] }
-
-          get_response  = get(route)
-          post_response = post(route)
-          t[get_response].should == t[post_response]
-        end
+        get_response  = get(route)
+        post_response = post(route)
+        t[get_response].should == t[post_response]
       end
     end
-
   end
 end
